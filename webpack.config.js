77e07909ac25d3ec4fc180dd7920936aeb68ee71
Василий Бардюков <ferisefbns@gmail.com>
2019-07-12 16:40:47 +0300

    Init for webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const path = require('path');
+const webpack = require('webpack');
+const webpackNodeExternals = require('webpack-node-externals');
+const dotenv = require('dotenv');
+
+const prod = process.env.NODE_ENV === 'production';
+
+if (!prod) {
+  dotenv.config();
+}
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  mode: prod ? 'production' : 'development',
+  output: {
+    path: path.resolve('./build'),
+    filename: 'server.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: [
+    webpackNodeExternals(),
+  ],
+  devtool: prod ? false : 'source-map',
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+};
