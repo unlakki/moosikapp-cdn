ed0d6d9e368c3b3bc80b1f21707cc21a03b292df
Ferisef <ferisefbns@gmail.com>
2020-01-05 13:07:07 +0300

    Add token manager

---
+++ src/utils/TokenManager.ts
@@ -0,0 +1,54 @@
+export interface JWTToken {
+  hex: string;
+  iat: number;
+  exp: number;
+}
+
+interface TokenManagerParams {
+  period: number;
+}
+
+export default class {
+  private period: number;
+
+  private tokenList = new Map<string, number>();
+
+  private timer: NodeJS.Timeout;
+
+  constructor({ period }: TokenManagerParams) {
+    this.period = period;
+    this.start();
+  }
+
+  public start() {
+    this.timer = setInterval(this.check, this.period);
+  }
+
+  public stop() {
+    clearInterval(this.timer);
+  }
+
+  public add(token: JWTToken) {
+    this.tokenList.set(token.hex, token.exp);
+  }
+
+  public has(token: JWTToken) {
+    return this.tokenList.has(token.hex);
+  }
+
+  private check() {
+    if (!this.tokenList) {
+      this.tokenList = new Map<string, number>();
+    }
+
+    this.tokenList = Object.entries(this.tokenList).reduce((list, token) => {
+      const now = Date.now();
+
+      if (now < token[1] * 1000) {
+        list.set(token[0], token[1]);
+      }
+
+      return list;
+    }, new Map<string, number>());
+  }
+}
