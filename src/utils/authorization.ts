00e7a49384f498055b4ed4f112d288b2e67c3548
uwufule <ferisefbns@gmail.com>
2020-02-11 22:14:38 +0300

    Update authorization

--- src/utils/authorization.ts
+++ src/utils/authorization.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express';
 import JWT from 'jsonwebtoken';
 import HTTPError from '../errors/HTTPError';
 
@@ -7,15 +8,24 @@ interface IAccessToken {
   role: number;
 }
 
-export default (accessToken: string): boolean => {
-  try {
-    const jwt = <IAccessToken>JWT.verify(accessToken, String(JWT_SECRET));
-    if (jwt.role < 1024) {
-      throw new Error();
-    }
+export default (req: Request): boolean => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    throw new HTTPError(401, 'Access denied.');
+  }
 
-    return true;
+  const accessToken = authorization.slice(7);
+
+  let jwt;
+  try {
+    jwt = <IAccessToken>JWT.verify(accessToken, String(JWT_SECRET));
   } catch (e) {
     throw new HTTPError(401, 'Access denied.');
   }
+
+  if (jwt.role < 1024) {
+    throw new HTTPError(401, 'Access denied.');
+  }
+
+  return true;
 };
