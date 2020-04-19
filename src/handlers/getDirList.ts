420c41e6bdb0a2152caf2e8ec1bb4395bbf55cd3
Ferisef <ferisefbns@gmail.com>
2020-04-19 16:17:48 +0300

    Remove list.ts

--- src/handlers/getDirList.ts
+++ src/handlers/getDirList.ts
@@ -1,13 +1,31 @@
 import { Request, Response } from 'express';
 import DiskManager from 'yadisk-mgr';
+import xml from 'xml';
+import filesize from 'filesize';
 import { createNotFoundError } from '../utils/errors';
 
 export default (diskManager: DiskManager) => async (req: Request, res: Response) => {
   const path = decodeURI(req.path);
 
   try {
-    const dirList = await diskManager.getDirList(path);
-    res.status(200).type('xml').send(list(dirList));
+    const items = await diskManager.getDirList(path);
+    res.status(200).type('xml').send(
+      xml({
+        ItemList: items.map((item) => ({
+          Item: [
+            {
+              Name: decodeURI(item.name),
+            },
+            {
+              Type: item.type,
+            },
+            {
+              Size: item.size ? filesize(item.size) : 'N/A',
+            },
+          ],
+        })),
+      }),
+    );
   } catch (e) {
     throw createNotFoundError(req.path);
   }
