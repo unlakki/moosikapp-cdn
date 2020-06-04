c78ecf4e49853f4964773c6923e80a7f5ec7bdd9
Ferisef <ferisefbns@gmail.com>
2020-06-04 23:44:41 +0300

    Fix

--- src/middlewares/permissions.ts
+++ src/middlewares/permissions.ts
@@ -1,11 +1,11 @@
 import { RequestHandler, Response, NextFunction } from 'express';
+import HttpErrors from 'http-errors';
 import { AuthorizedRequest } from './authorization';
-import { createForbittenError } from '../utils/errors';
 
 export default (minimunRequiredRole: number): RequestHandler => (
   (req: AuthorizedRequest, res: Response, next: NextFunction) => {
-    if (req.jwt.role < minimunRequiredRole) {
-      throw createForbittenError(req.path);
+    if (req.user.role < minimunRequiredRole) {
+      throw new HttpErrors.Forbidden();
     }
 
     next();
